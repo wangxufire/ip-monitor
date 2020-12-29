074aedd9b6cece0e1666725eaaa895afb2be0345
星野はじめ <wangxufire@gmail.com>
2020-12-29 12:13:38 +0800

    update

--- main.go
+++ main.go
@@ -2,65 +2,113 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
+var (
+	barkCode string
+	homeDir  string
+)
+
+func init() {
+	bark := flag.String("n", "2qT6qyWRNfAYYZx8sBsje7", "-bark ${bark_device_code}")
+	flag.Parse()
+	barkCode = *bark
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	homeDir = home
+}
+
 func main() {
 	for {
 		ip, err := getExternalIP()
 		if err != nil {
-			fmt.Println(err)
+			log(err)
+			sleep()
 			continue
 		}
-		fmt.Printf("fetch ip success %s", ip)
-		ipFile := "/var/log/current-ip"
+		log(fmt.Sprintf("fetch ip success %s\n", ip))
+		ipFile := homeDir + "/.current-ip"
 		_, err = os.Stat(ipFile)
 		if os.IsNotExist(err) {
 			f, err := os.Create(ipFile)
 			if err != nil {
-				fmt.Println(err)
+				log(err)
+				sleep()
 				continue
 			}
 			defer f.Close()
 			f.WriteString(ip)
 			f.Sync()
-		} else {
-			f, err := os.OpenFile(ipFile, os.O_APPEND, 0666)
+			continue
+		}
+		if err != nil {
+			log(err)
+			sleep()
+			continue
+		}
+		f, err := os.OpenFile(ipFile, os.O_APPEND, os.ModePerm)
+		if err != nil {
+			log(err)
+			sleep()
+			continue
+		}
+		defer f.Close()
+		buf, err := ioutil.ReadFile(f.Name())
+		if err != nil {
+			log(err)
+			sleep()
+			continue
+		}
+		oldIP := string(buf)
+		if strings.Compare(ip, oldIP) != 0 {
+			err = notify(ip)
 			if err != nil {
-				fmt.Println(err)
+				log(err)
+				sleep()
 				continue
 			}
-			defer f.Close()
-			content := make([]byte, 10)
-			f.Read(content)
-			if ip != string(content) {
-				url := fmt.Sprintf("https://api.day.app/2qT6qyWRNAYnYZx8mBsje7/ip-change/%s?isArchive=1&sound=minuet", ip)
-				request, err := http.NewRequest("GET", url, nil)
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				defer request.Body.Close()
-				request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-				client := &http.Client{}
-				response, err := client.Do(request)
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				defer response.Body.Close()
-				f.Truncate(int64(len(content)))
-				f.WriteString(ip)
-				f.Sync()
-			}
+			f.Truncate(int64(len(buf)))
+			f.WriteString(ip)
+			f.Sync()
 		}
-		time.Sleep(10 * time.Minute)
+		sleep()
 	}
+}
+
+func sleep() {
+	time.Sleep(10 * time.Minute)
+}
+
+func log(msg interface{}) {
+	date := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("%s %+v \n", date, msg)
+}
+
+func notify(ip string) error {
+	url := fmt.Sprintf("https://api.day.app/%s/ip-change/%s?isArchive=1&sound=birdsong", barkCode, ip)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	log("notify ip changed success")
+	return nil
 }
 
 func getExternalIP() (string, error) {
@@ -68,10 +116,8 @@ func getExternalIP() (string, error) {
 	if err != nil {
 		return "", errors.New("external IP fetch failed, detail:" + err.Error())
 	}
-
 	defer response.Body.Close()
 	res := ""
-
 	for {
 		tmp := make([]byte, 32)
 		n, err := response.Body.Read(tmp)
